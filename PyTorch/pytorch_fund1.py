cfc5aadc3e360234e41a0657b2d51658a1a5bff9
Sean Ken Ruiz <seanruiz0@gmail.com>
2024-09-13 13:21:56 -0700

    learning!

---
+++ PyTorch/pytorch_fund1.py
@@ -0,0 +1,14 @@
+import torch
+from torch import nn # nn contains all of PyTorch's building blocks for neural networks
+import matplotlib.pyplot as plt
+
+what_were_covering = {1: "data (prepare and load)",
+                      2: "build model",
+                      3: "fitting the model to data (training)",
+                      4: "making predictions and evaluating a model (inference)",
+                      5: "saving and loading a model",
+                      6: "putting it all together"
+                      }
+
+# Check PyTorch version
+print(torch.__version__)
